751d1f6d89e269deb69fb5a2facca9d093debba7
mcally <mcallydevelops@gmail.com>
2021-11-16 23:04:02 -0600

    initial commit

---
+++ src/test/java/com/mcally/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.mcally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
